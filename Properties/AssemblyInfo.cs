fed32682cd31072ad3a012fda2ffddb583e4d829
Ephemerality <revensoftware+github@gmail.com>
2018-05-28 20:29:01 -0400

    Skipped a version at some point. Also bump copyright year.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("X-Ray Builder GUI")]
 [assembly: AssemblyProduct("X-Ray Builder GUI")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.23.0")]
-[assembly: AssemblyFileVersion("2.0.23.0")]
+[assembly: AssemblyVersion("2.0.24.0")]
+[assembly: AssemblyFileVersion("2.0.24.0")]

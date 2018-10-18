2b32106b1d7db379daed2847fe89ffac31632ae6
Ephemerality <revensoftware+github@gmail.com>
2018-10-18 14:29:32 -0400

    convert datasource to interface

--- src/DataSources/Secondary/Model/ISecondarySource.cs
+++ src/DataSources/Secondary/Model/ISecondarySource.cs
@@ -1,29 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 
 namespace XRayBuilderGUI.DataSources
 {
-    public abstract class DataSource
+    public interface ISecondarySource
     {
-        public abstract string Name { get; }
-        public virtual HtmlDocument sourceHtmlDoc { get; set; }
-        public abstract Task<List<BookInfo>> SearchBook(string author, string title);
-        public abstract Task<BookInfo> GetNextInSeries(BookInfo curBook, AuthorProfile authorProfile, string TLD);
-        public virtual Task<bool> GetPageCount(BookInfo curBook) { return Task.FromResult(false); }
-        public virtual Task GetExtras(BookInfo curBook, CancellationToken token, IProgressBar progress = null) { return Task.FromResult(false); }
-        public virtual Task<List<XRay.Term>> GetTerms(string dataUrl, IProgressBar progress, CancellationToken token) { return Task.FromResult(new List<XRay.Term>()); }
-        public virtual Task<List<NotableClip>> GetNotableClips(string url, CancellationToken token, HtmlDocument srcDoc = null, IProgressBar progress = null) { return Task.FromResult(new List<NotableClip>()); }
-        
-        public class FormatChangedException : Exception
-        {
-            public FormatChangedException(string source, string message, Exception previous = null)
-                : base($"Format changed on {source} ({message}).", previous)
-            {
-            }
-        }
+        Task<IEnumerable<BookInfo>> SearchBookAsync(string author, string title, CancellationToken cancellationToken = default);
+        Task<BookInfo> GetNextInSeriesAsync(BookInfo curBook, AuthorProfile authorProfile, string TLD, CancellationToken cancellationToken = default);
+        Task<bool> GetPageCountAsync(BookInfo curBook, CancellationToken cancellationToken = default);
+        Task GetExtrasAsync(BookInfo curBook, IProgressBar progress = null, CancellationToken cancellationToken = default);
+        Task<IEnumerable<XRay.Term>> GetTermsAsync(string dataUrl, IProgressBar progress, CancellationToken cancellationToken = default);
+        Task<IEnumerable<NotableClip>> GetNotableClipsAsync(string url, HtmlDocument srcDoc = null, IProgressBar progress = null, CancellationToken cancellationToken = default);
     }
 
     public class NotableClip

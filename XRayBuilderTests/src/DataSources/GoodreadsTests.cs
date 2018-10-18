5c733ddcc96c4c8897fe9f118944d3745f1bc6af
Ephemerality <revensoftware+github@gmail.com>
2018-10-17 22:41:43 -0400

    remove empty tests for now

--- XRayBuilderTests/src/DataSources/GoodreadsTests.cs
+++ XRayBuilderTests/src/DataSources/GoodreadsTests.cs
@@ -2,43 +2,43 @@
 
 namespace XRayBuilderTests.DataSources
 {
-    [TestFixture()]
-    public class GoodreadsTests
-    {
-        [Test()]
-        public void SearchBookTest()
-        {
-            Assert.Fail();
-        }
+    //[TestFixture()]
+    //public class GoodreadsTests
+    //{
+    //    [Test()]
+    //    public void SearchBookTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetNextInSeriesTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetNextInSeriesTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetPageCountTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetPageCountTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetTermsTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetTermsTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetNotableClipsTest()
-        {
-            Assert.Fail();
-        }
+    //    [Test()]
+    //    public void GetNotableClipsTest()
+    //    {
+    //        Assert.Fail();
+    //    }
 
-        [Test()]
-        public void GetExtrasTest()
-        {
-            Assert.Fail();
-        }
-    }
+    //    [Test()]
+    //    public void GetExtrasTest()
+    //    {
+    //        Assert.Fail();
+    //    }
+    //}
 }

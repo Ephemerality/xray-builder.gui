ac4f73a9fd1cda5e9d9c97969337166352aea4e2
Ephemerality <revensoftware+github@gmail.com>
2019-05-11 14:25:17 -0300

    migrate project format

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("X-Ray Builder GUI")]
-[assembly: AssemblyDescription("Generate X-Ray, Start Action, Author Profile and End Action files for use on your Amazon Kindle.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("X-Ray Builder GUI")]
-[assembly: AssemblyProduct("X-Ray Builder GUI")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,19 +9,6 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("55a9e9b9-405f-446e-8d5c-833ed1979bc1")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
 
 // Make internals visible to test project
 [assembly: InternalsVisibleTo("XRayBuilderTests")]

07259ef6a067a6e34d511b8911216e7b0070b403
Ephemerality <revensoftware+github@gmail.com>
2019-05-20 18:06:44 -0300

    auto-registration

--- XRayBuilder/src/Program.cs
+++ XRayBuilder/src/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Windows.Forms;
 using SimpleInjector;
-using SimpleInjector.Diagnostics;
 using XRayBuilderGUI.DataSources.Amazon;
 using XRayBuilderGUI.DataSources.Secondary;
 using XRayBuilderGUI.UI;
@@ -31,36 +30,19 @@ namespace XRayBuilderGUI
             _container = new Container();
 
             _container.Register<ILogger, Logger>(Lifestyle.Singleton);
-            _container.Register<frmMain>();
-            var registration = _container.GetRegistration(typeof(frmMain)).Registration;
-            registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Disposed by application");
+            _container.RegisterTransientIgnore<frmMain>("Disposed by application");
 
             _container.Register<IHttpClient, HttpClient>(Lifestyle.Singleton);
             _container.Register<IAmazonClient, AmazonClient>(Lifestyle.Singleton);
             _container.Register<IAuthorProfileGenerator, AuthorProfileGenerator>(Lifestyle.Singleton);
 
-            // TODO: Autoregister Factory<>, SecondaryDataSources, PreviewProviders
+            // TODO: Figure out autoregister for Factory<,>
             _container.Register<SecondaryDataSourceFactory>(Lifestyle.Singleton);
-            _container.Register<Shelfari>(Lifestyle.Singleton);
-            _container.Register<Goodreads>(Lifestyle.Singleton);
+            _container.AutoregisterConcreteFromInterface<ISecondarySource>(Lifestyle.Singleton);
 
             _container.Register<PreviewProviderFactory>(Lifestyle.Singleton);
-            _container.Register<PreviewProvider.PreviewProviderAuthorProfile>(Lifestyle.Singleton);
-            _container.Register<PreviewProvider.PreviewProviderEndActions>(Lifestyle.Singleton);
-            _container.Register<PreviewProvider.PreviewProviderStartActions>(Lifestyle.Singleton);
-            _container.Register<PreviewProvider.PreviewProviderXRay>(Lifestyle.Singleton);
-            _container.Register<frmPreviewAP>();
-            _container.Register<frmPreviewEA>();
-            _container.Register<frmPreviewSA>();
-            _container.Register<frmPreviewXR>();
-            registration = _container.GetRegistration(typeof(frmPreviewAP)).Registration;
-            registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Manually disposed");
-            registration = _container.GetRegistration(typeof(frmPreviewEA)).Registration;
-            registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Manually disposed");
-            registration = _container.GetRegistration(typeof(frmPreviewSA)).Registration;
-            registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Manually disposed");
-            registration = _container.GetRegistration(typeof(frmPreviewXR)).Registration;
-            registration.SuppressDiagnosticWarning(DiagnosticType.DisposableTransientComponent, "Manually disposed");
+            _container.AutoregisterConcreteFromAbstract<PreviewProvider>(Lifestyle.Singleton);
+            _container.AutoregisterDisposableTransientConcreteFromInterface<IPreviewForm>("Manually disposed");
 
             _container.Verify();
         }

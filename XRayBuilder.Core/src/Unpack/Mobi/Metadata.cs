3ae292f5b78bc27633d7a52f750eadcfba6eeb61
Ephemerality <revensoftware+github@gmail.com>
2019-11-18 18:29:42 -0400

    bring back old unpack with buffer array, can't trust textlength from palmdocheader

--- XRayBuilder.Core/src/Unpack/Mobi/Metadata.cs
+++ XRayBuilder.Core/src/Unpack/Mobi/Metadata.cs
@@ -172,16 +172,18 @@ namespace XRayBuilder.Core.Unpack.Mobi
             };
 
             decomp.Initialize(_mobiHeader, _pdb, _headerRecords);
-            var rawMl = new byte[_activePdh.TextLength];
-            var rawMlOffset = 0;
+            var rawMl = new byte[0];
             var endRecord = _startRecord + _activePdh.RecordCount - 1;
             for (var i = _startRecord; i <= endRecord; i++)
             {
                 var headerBuffer = TrimTrailingDataEntries(_headerRecords[i]);
                 var result = decomp.Unpack(headerBuffer);
-                Buffer.BlockCopy(result, 0, rawMl, rawMlOffset, result.Length);
-                rawMlOffset += result.Length;
+                var buffer = new byte[rawMl.Length + result.Length];
+                Buffer.BlockCopy(rawMl, 0, buffer, 0, rawMl.Length);
+                Buffer.BlockCopy(result, 0, buffer, rawMl.Length, result.Length);
+                rawMl = buffer;
             }
+
             return rawMl;
         }
 

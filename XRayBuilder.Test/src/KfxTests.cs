243375a2465b1b1ed0f92ba0dc587205e4f06283
Ephemerality <revensoftware+github@gmail.com>
2020-04-16 21:30:20 -0300

    update tests

--- XRayBuilder.Test/src/KfxTests.cs
+++ XRayBuilder.Test/src/KfxTests.cs
@@ -1,4 +1,7 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using NUnit.Framework;
 using XRayBuilder.Core.Unpack.KFX;
 
@@ -28,6 +31,7 @@ namespace XRayBuilder.Test
             Assert.AreEqual("Frankenstein", kfx.Title);
         }
 
+        [TestCase(@"testfiles\Fire & Blood (A Song of Ice and - George R. R. Martin.kfx", 33)]
         [TestCase(@"testfiles\Frankenstein - Mary W. Shelley.kfx", 29)]
         public void DefaultTocTest(string kfxFile, int tocLength)
         {
@@ -38,7 +42,33 @@ namespace XRayBuilder.Test
             Assert.AreEqual(tocLength, toc.Count);
         }
 
-        //// TODO: Python handles 146 and 176 when getting content too
+        [TestCase(@"testfiles\Fire & Blood (A Song of Ice and - George R. R. Martin.kfx", "Baelon", 6213, 799606, 3274, 34956246)]
+        [TestCase(@"testfiles\Frankenstein - Mary W. Shelley.kfx", "Frankenstein", 3, 415895, 757, 6457329)]
+        public void ContentTest(string kfxFile, string search, int firstOffset, int lastOffset, int chunkCount, long sum)
+        {
+            var fs = new FileStream(kfxFile, FileMode.Open, FileAccess.Read);
+            var kfx = new KfxContainer(fs);
+            var contentChunks = kfx.GetContentChunks();
+            var testSearch = FindInChunks(contentChunks, search).ToArray();
+
+            Assert.AreEqual(chunkCount, contentChunks.Count);
+            Assert.AreEqual(firstOffset, testSearch.First());
+            Assert.AreEqual(lastOffset, testSearch.Last());
+            Assert.AreEqual(sum, testSearch.Sum());
+        }
+
+        private IEnumerable<long> FindInChunks(IEnumerable<ContentChunk> chunks, string search)
+        {
+            var offset = 0L;
+            foreach (var chunk in chunks)
+            {
+                if (chunk.ContentText != null && chunk.ContentText.Contains(search))
+                    yield return offset + chunk.ContentText.IndexOf(search, StringComparison.Ordinal);
+
+                offset += chunk.Length;
+            }
+        }
+
         // var content1 = kfx.Entities.Where(fragment => fragment.FragmentType == KfxSymbols.Content);
         // var content2 = content1
         //     .Select(frag => frag.Value).OfType<IonStruct>()
@@ -47,18 +77,21 @@ namespace XRayBuilder.Test
         //     .Select(para => para.StringValue)
         //     .ToArray();
 
-        //var offset = 0;
-        //foreach (var (content, index) in content2.Select((c, i) => (c, i)))
-        //{
-        //    var tests = content;
-        //    var test2 = Encoding.UTF8.GetBytes(tests);
-        //    if (tests.Contains("by the bitterest remorse"))
-        //        continue;
-        //    if (index == content2.Length - 1)
-        //        continue;
-        //    if (test2.Length != content.Length)
-        //        continue;
-        //    offset += test2.Length;
-        //}
+        // offset = 0;
+        // foreach (var (content, index) in content2.Select((c, i) => (c, i)))
+        // {
+        //     var tests = content;
+        //     var test2 = Encoding.UTF8.GetBytes(tests);
+        //     if (tests.Contains("Baelon"))
+        //     {
+        //         var result = offset + tests.IndexOf("Baelon", StringComparison.Ordinal);
+        //         continue;
+        //     }
+        //     if (index == content2.Length - 1)
+        //         continue;
+        //     // if (test2.Length != content.Length)
+        //     //     continue;
+        //     offset += tests.Length;
+        // }
     }
 }

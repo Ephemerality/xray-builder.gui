ac01948b2b2f38b4d1ee15774d9d1623be97ce17
Ephemerality <revensoftware+github@gmail.com>
2020-04-22 00:25:02 -0300

    regen form

--- XRayBuilder/src/UI/frmCreateXR.designer.cs
+++ XRayBuilder/src/UI/frmCreateXR.designer.cs
@@ -30,7 +30,7 @@
         {
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCreateXR));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XRayBuilderGUI.UI.frmCreateXR));
             this.lblName = new System.Windows.Forms.Label();
             this.lblAliases = new System.Windows.Forms.Label();
             this.rdoWikipedia = new System.Windows.Forms.RadioButton();
@@ -88,9 +88,6 @@
             this.groupBox5.SuspendLayout();
             this.cmsTerms.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // lblName
-            // 
             this.lblName.AutoSize = true;
             this.lblName.Location = new System.Drawing.Point(10, 23);
             this.lblName.Name = "lblName";
@@ -98,9 +95,6 @@
             this.lblName.TabIndex = 1;
             this.lblName.Text = "Name:";
             this.lblName.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // lblAliases
-            // 
             this.lblAliases.AutoSize = true;
             this.lblAliases.Location = new System.Drawing.Point(11, 55);
             this.lblAliases.Name = "lblAliases";
@@ -108,9 +102,6 @@
             this.lblAliases.TabIndex = 4;
             this.lblAliases.Text = "Aliases:";
             this.lblAliases.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // rdoWikipedia
-            // 
             this.rdoWikipedia.AutoSize = true;
             this.rdoWikipedia.Location = new System.Drawing.Point(95, 17);
             this.rdoWikipedia.Name = "rdoWikipedia";
@@ -118,9 +109,6 @@
             this.rdoWikipedia.TabIndex = 1;
             this.rdoWikipedia.Text = "Wikipedia";
             this.rdoWikipedia.UseVisualStyleBackColor = true;
-            // 
-            // rdoGoodreads
-            // 
             this.rdoGoodreads.AutoSize = true;
             this.rdoGoodreads.Checked = true;
             this.rdoGoodreads.Location = new System.Drawing.Point(14, 17);
@@ -130,9 +118,6 @@
             this.rdoGoodreads.TabStop = true;
             this.rdoGoodreads.Text = "Goodreads";
             this.rdoGoodreads.UseVisualStyleBackColor = true;
-            // 
-            // groupBox3
-            // 
             this.groupBox3.Controls.Add(this.btnLink);
             this.groupBox3.Controls.Add(this.rdoTopic);
             this.groupBox3.Controls.Add(this.rdoCharacter);
@@ -150,9 +135,6 @@
             this.groupBox3.TabIndex = 39;
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Details";
-            // 
-            // btnLink
-            // 
             this.btnLink.Location = new System.Drawing.Point(491, 150);
             this.btnLink.Name = "btnLink";
             this.btnLink.Size = new System.Drawing.Size(31, 26);
@@ -160,9 +142,6 @@
             this.btnLink.Text = "...";
             this.btnLink.UseVisualStyleBackColor = true;
             this.btnLink.Click += new System.EventHandler(this.btnLink_Click);
-            // 
-            // rdoTopic
-            // 
             this.rdoTopic.AutoSize = true;
             this.rdoTopic.Location = new System.Drawing.Point(471, 22);
             this.rdoTopic.Name = "rdoTopic";
@@ -170,9 +149,6 @@
             this.rdoTopic.TabIndex = 3;
             this.rdoTopic.Text = "Setting";
             this.rdoTopic.UseVisualStyleBackColor = true;
-            // 
-            // rdoCharacter
-            // 
             this.rdoCharacter.AutoSize = true;
             this.rdoCharacter.Checked = true;
             this.rdoCharacter.Location = new System.Drawing.Point(394, 22);
@@ -182,16 +158,10 @@
             this.rdoCharacter.TabStop = true;
             this.rdoCharacter.Text = "Character";
             this.rdoCharacter.UseVisualStyleBackColor = true;
-            // 
-            // txtLink
-            // 
             this.txtLink.Location = new System.Drawing.Point(78, 156);
             this.txtLink.Name = "txtLink";
             this.txtLink.Size = new System.Drawing.Size(403, 20);
             this.txtLink.TabIndex = 9;
-            // 
-            // lblLink
-            // 
             this.lblLink.AutoSize = true;
             this.lblLink.Location = new System.Drawing.Point(11, 159);
             this.lblLink.Name = "lblLink";
@@ -199,17 +169,11 @@
             this.lblLink.TabIndex = 8;
             this.lblLink.Text = "Web Link:";
             this.lblLink.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // txtDescription
-            // 
             this.txtDescription.Location = new System.Drawing.Point(79, 84);
             this.txtDescription.Multiline = true;
             this.txtDescription.Name = "txtDescription";
             this.txtDescription.Size = new System.Drawing.Size(443, 60);
             this.txtDescription.TabIndex = 7;
-            // 
-            // lblDescription
-            // 
             this.lblDescription.AutoSize = true;
             this.lblDescription.Location = new System.Drawing.Point(10, 87);
             this.lblDescription.Name = "lblDescription";
@@ -217,23 +181,14 @@
             this.lblDescription.TabIndex = 6;
             this.lblDescription.Text = "Description:";
             this.lblDescription.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // txtAliases
-            // 
             this.txtAliases.Location = new System.Drawing.Point(78, 52);
             this.txtAliases.Name = "txtAliases";
             this.txtAliases.Size = new System.Drawing.Size(444, 20);
             this.txtAliases.TabIndex = 5;
-            // 
-            // txtName
-            // 
             this.txtName.Location = new System.Drawing.Point(79, 20);
             this.txtName.Name = "txtName";
             this.txtName.Size = new System.Drawing.Size(303, 20);
             this.txtName.TabIndex = 1;
-            // 
-            // groupBox2
-            // 
             this.groupBox2.Controls.Add(this.chkDelete);
             this.groupBox2.Controls.Add(this.chkRegex);
             this.groupBox2.Controls.Add(this.chkMatch);
@@ -244,9 +199,6 @@
             this.groupBox2.TabIndex = 40;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Options";
-            // 
-            // chkDelete
-            // 
             this.chkDelete.AutoSize = true;
             this.chkDelete.Enabled = false;
             this.chkDelete.Location = new System.Drawing.Point(184, 19);
@@ -255,9 +207,6 @@
             this.chkDelete.TabIndex = 3;
             this.chkDelete.Text = "Delete";
             this.chkDelete.UseVisualStyleBackColor = true;
-            // 
-            // chkRegex
-            // 
             this.chkRegex.AutoSize = true;
             this.chkRegex.Enabled = false;
             this.chkRegex.Location = new System.Drawing.Point(244, 19);
@@ -266,9 +215,6 @@
             this.chkRegex.TabIndex = 2;
             this.chkRegex.Text = "RegEx";
             this.chkRegex.UseVisualStyleBackColor = true;
-            // 
-            // chkMatch
-            // 
             this.chkMatch.AutoSize = true;
             this.chkMatch.Checked = true;
             this.chkMatch.CheckState = System.Windows.Forms.CheckState.Checked;
@@ -278,9 +224,6 @@
             this.chkMatch.TabIndex = 1;
             this.chkMatch.Text = "Match";
             this.chkMatch.UseVisualStyleBackColor = true;
-            // 
-            // chkCase
-            // 
             this.chkCase.AutoSize = true;
             this.chkCase.Checked = true;
             this.chkCase.CheckState = System.Windows.Forms.CheckState.Checked;
@@ -290,9 +233,6 @@
             this.chkCase.TabIndex = 0;
             this.chkCase.Text = "Case Sensitive";
             this.chkCase.UseVisualStyleBackColor = true;
-            // 
-            // groupBox4
-            // 
             this.groupBox4.Controls.Add(this.label1);
             this.groupBox4.Controls.Add(this.dgvTerms);
             this.groupBox4.Location = new System.Drawing.Point(12, 255);
@@ -301,17 +241,11 @@
             this.groupBox4.TabIndex = 46;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Terms";
-            // 
-            // label1
-            // 
             this.label1.Location = new System.Drawing.Point(14, 16);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(507, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Double-click an item to remove it and edit it above. Don\'t forget to add it back " + "in!";
-            // 
-            // dgvTerms
-            // 
+            this.label1.Text = ("Double-click an item to remove it and edit it above. Don\'t forget to add it back " + "" + "in!");
             this.dgvTerms.AllowUserToAddRows = false;
             this.dgvTerms.AllowUserToDeleteRows = false;
             this.dgvTerms.AllowUserToResizeRows = false;
@@ -331,80 +265,47 @@
             this.dgvTerms.TabIndex = 0;
             this.dgvTerms.CellMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvTerms_CellMouseDoubleClick);
             this.dgvTerms.CellMouseDown += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvTerms_CellMouseDown);
-            // 
-            // Column1
-            // 
             this.Column1.HeaderText = "Type";
             this.Column1.Name = "Column1";
             this.Column1.ReadOnly = true;
             this.Column1.Width = 36;
-            // 
-            // Column2
-            // 
             this.Column2.HeaderText = "Name";
             this.Column2.Name = "Column2";
             this.Column2.ReadOnly = true;
-            // 
-            // Column3
-            // 
             this.Column3.HeaderText = "Aliases";
             this.Column3.Name = "Column3";
             this.Column3.ReadOnly = true;
             this.Column3.Width = 150;
-            // 
-            // Column4
-            // 
             this.Column4.HeaderText = "Description";
             this.Column4.Name = "Column4";
             this.Column4.ReadOnly = true;
             this.Column4.Width = 150;
-            // 
-            // Column6
-            // 
             this.Column6.HeaderText = "URL";
             this.Column6.Name = "Column6";
             this.Column6.ReadOnly = true;
-            // 
-            // Column10
-            // 
             dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             this.Column10.DefaultCellStyle = dataGridViewCellStyle1;
             this.Column10.HeaderText = "Source";
             this.Column10.Name = "Column10";
             this.Column10.ReadOnly = true;
             this.Column10.Width = 70;
-            // 
-            // Column5
-            // 
             this.Column5.HeaderText = "Match";
             this.Column5.Name = "Column5";
             this.Column5.ReadOnly = true;
             this.Column5.Resizable = System.Windows.Forms.DataGridViewTriState.True;
             this.Column5.Width = 90;
-            // 
-            // Column7
-            // 
             this.Column7.HeaderText = "Case Sensitive";
             this.Column7.Name = "Column7";
             this.Column7.ReadOnly = true;
             this.Column7.Width = 90;
-            // 
-            // Column8
-            // 
             this.Column8.HeaderText = "Delete";
             this.Column8.Name = "Column8";
             this.Column8.ReadOnly = true;
             this.Column8.Width = 90;
-            // 
-            // Column9
-            // 
             this.Column9.HeaderText = "RegEx";
             this.Column9.Name = "Column9";
             this.Column9.ReadOnly = true;
             this.Column9.Width = 90;
-            // 
-            // groupBox1
-            // 
             this.groupBox1.Controls.Add(this.rdoWikipedia);
             this.groupBox1.Controls.Add(this.rdoGoodreads);
             this.groupBox1.Location = new System.Drawing.Point(12, 202);
@@ -413,9 +314,6 @@
             this.groupBox1.TabIndex = 30;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Source";
-            // 
-            // groupBox5
-            // 
             this.groupBox5.Controls.Add(this.txtAsin);
             this.groupBox5.Controls.Add(this.lblAsin);
             this.groupBox5.Controls.Add(this.txtTitle);
@@ -428,16 +326,10 @@
             this.groupBox5.TabIndex = 48;
             this.groupBox5.TabStop = false;
             this.groupBox5.Text = "Save File As";
-            // 
-            // txtAsin
-            // 
             this.txtAsin.Location = new System.Drawing.Point(442, 20);
             this.txtAsin.Name = "txtAsin";
             this.txtAsin.Size = new System.Drawing.Size(80, 20);
             this.txtAsin.TabIndex = 6;
-            // 
-            // lblAsin
-            // 
             this.lblAsin.AutoSize = true;
             this.lblAsin.Location = new System.Drawing.Point(400, 23);
             this.lblAsin.Name = "lblAsin";
@@ -445,16 +337,10 @@
             this.lblAsin.TabIndex = 7;
             this.lblAsin.Text = "ASIN:";
             this.lblAsin.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // txtTitle
-            // 
             this.txtTitle.Location = new System.Drawing.Point(246, 20);
             this.txtTitle.Name = "txtTitle";
             this.txtTitle.Size = new System.Drawing.Size(145, 20);
             this.txtTitle.TabIndex = 4;
-            // 
-            // lblTitle
-            // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Location = new System.Drawing.Point(211, 23);
             this.lblTitle.Name = "lblTitle";
@@ -462,16 +348,10 @@
             this.lblTitle.TabIndex = 5;
             this.lblTitle.Text = "Tite:";
             this.lblTitle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // txtAuthor
-            // 
             this.txtAuthor.Location = new System.Drawing.Point(57, 20);
             this.txtAuthor.Name = "txtAuthor";
             this.txtAuthor.Size = new System.Drawing.Size(145, 20);
             this.txtAuthor.TabIndex = 2;
-            // 
-            // lblAuthor
-            // 
             this.lblAuthor.AutoSize = true;
             this.lblAuthor.Location = new System.Drawing.Point(9, 23);
             this.lblAuthor.Name = "lblAuthor";
@@ -479,9 +359,6 @@
             this.lblAuthor.TabIndex = 3;
             this.lblAuthor.Text = "Author:";
             this.lblAuthor.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
-            // btnOpenXml
-            // 
             this.btnOpenXml.Image = ((System.Drawing.Image) (resources.GetObject("btnOpenXml.Image")));
             this.btnOpenXml.Location = new System.Drawing.Point(559, 398);
             this.btnOpenXml.Name = "btnOpenXml";
@@ -489,9 +366,6 @@
             this.btnOpenXml.TabIndex = 47;
             this.btnOpenXml.UseVisualStyleBackColor = true;
             this.btnOpenXml.Click += new System.EventHandler(this.btnOpenXml_Click);
-            // 
-            // btnEditTerm
-            // 
             this.btnEditTerm.Image = ((System.Drawing.Image) (resources.GetObject("btnEditTerm.Image")));
             this.btnEditTerm.Location = new System.Drawing.Point(559, 260);
             this.btnEditTerm.Name = "btnEditTerm";
@@ -499,9 +373,6 @@
             this.btnEditTerm.TabIndex = 44;
             this.btnEditTerm.UseVisualStyleBackColor = true;
             this.btnEditTerm.Click += new System.EventHandler(this.btnEditTerm_Click);
-            // 
-            // btnSaveXML
-            // 
             this.btnSaveXML.Image = ((System.Drawing.Image) (resources.GetObject("btnSaveXML.Image")));
             this.btnSaveXML.Location = new System.Drawing.Point(559, 440);
             this.btnSaveXML.Name = "btnSaveXML";
@@ -509,9 +380,6 @@
             this.btnSaveXML.TabIndex = 43;
             this.btnSaveXML.UseVisualStyleBackColor = true;
             this.btnSaveXML.Click += new System.EventHandler(this.btnSaveXML_Click);
-            // 
-            // btnRemoveTerm
-            // 
             this.btnRemoveTerm.Image = ((System.Drawing.Image) (resources.GetObject("btnRemoveTerm.Image")));
             this.btnRemoveTerm.Location = new System.Drawing.Point(559, 302);
             this.btnRemoveTerm.Name = "btnRemoveTerm";
@@ -519,9 +387,6 @@
             this.btnRemoveTerm.TabIndex = 42;
             this.btnRemoveTerm.UseVisualStyleBackColor = true;
             this.btnRemoveTerm.Click += new System.EventHandler(this.btnRemoveTerm_Click);
-            // 
-            // btnAddTerm
-            // 
             this.btnAddTerm.Image = ((System.Drawing.Image) (resources.GetObject("btnAddTerm.Image")));
             this.btnAddTerm.Location = new System.Drawing.Point(559, 11);
             this.btnAddTerm.Name = "btnAddTerm";
@@ -529,17 +394,11 @@
             this.btnAddTerm.TabIndex = 41;
             this.btnAddTerm.UseVisualStyleBackColor = true;
             this.btnAddTerm.Click += new System.EventHandler(this.btnAddTerm_Click);
-            // 
-            // cmsTerms
-            // 
             this.cmsTerms.AutoSize = false;
             this.cmsTerms.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
             this.cmsTerms.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {this.tsmEdit, this.tsmDelete});
             this.cmsTerms.Name = "cmsTerms";
             this.cmsTerms.Size = new System.Drawing.Size(80, 48);
-            // 
-            // tsmEdit
-            // 
             this.tsmEdit.AutoSize = false;
             this.tsmEdit.Image = ((System.Drawing.Image) (resources.GetObject("tsmEdit.Image")));
             this.tsmEdit.Name = "tsmEdit";
@@ -547,18 +406,12 @@
             this.tsmEdit.Size = new System.Drawing.Size(79, 22);
             this.tsmEdit.Text = "Edit";
             this.tsmEdit.Click += new System.EventHandler(this.tsmEdit_Click);
-            // 
-            // tsmDelete
-            // 
             this.tsmDelete.AutoSize = false;
             this.tsmDelete.Image = ((System.Drawing.Image) (resources.GetObject("tsmDelete.Image")));
             this.tsmDelete.Name = "tsmDelete";
             this.tsmDelete.Size = new System.Drawing.Size(79, 22);
             this.tsmDelete.Text = "Delete";
             this.tsmDelete.Click += new System.EventHandler(this.tsmDelete_Click);
-            // 
-            // btnClear
-            // 
             this.btnClear.Image = ((System.Drawing.Image) (resources.GetObject("btnClear.Image")));
             this.btnClear.Location = new System.Drawing.Point(559, 344);
             this.btnClear.Name = "btnClear";
@@ -566,9 +419,6 @@
             this.btnClear.TabIndex = 49;
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
-            // 
-            // frmCreateXR
-            // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(602, 511);

beda121be1a06dfa46b03bbcde1ca7d6478d456b
Ephemerality <revensoftware+github@gmail.com>
2020-05-07 16:44:50 -0300

    download all pages in utf8

--- XRayBuilder.Core/src/Libraries/Http/HttpClient.cs
+++ XRayBuilder.Core/src/Libraries/Http/HttpClient.cs
@@ -40,7 +40,7 @@ namespace XRayBuilder.Core.Libraries.Http
         {
             var htmlDoc = new HtmlDocument {OptionAutoCloseOnEnd = true};
             var stream = await GetStreamAsync(url, cancellationToken);
-            htmlDoc.Load(stream);
+            htmlDoc.Load(stream, Encoding.UTF8);
             return htmlDoc;
         }
 

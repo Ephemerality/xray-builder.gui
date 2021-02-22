46ac5158e2733b228843389874687d9b3bf6de98
PsychoReptile <darrenmcg@hotmail.com>
2021-02-22 10:39:29 +0000

    Default download next in series changed to enabled.

--- XRayBuilder/Properties/Settings.Designer.cs
+++ XRayBuilder/Properties/Settings.Designer.cs
@@ -505,7 +505,7 @@ namespace XRayBuilderGUI.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool downloadNIS {
             get {
                 return ((bool)(this["downloadNIS"]));

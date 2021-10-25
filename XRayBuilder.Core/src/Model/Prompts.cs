9bb1d867ebb8cc17225098a714bfd882abb99319
Ephemerality <revensoftware+github@gmail.com>
2021-10-24 22:20:06 -0300

    generic prompts

---
+++ XRayBuilder.Core/src/Model/Prompts.cs
@@ -0,0 +1,26 @@
+﻿namespace XRayBuilder.Core.Model
+{
+    public delegate PromptResultYesNo YesNoPrompt(string title, string message, PromptType type);
+
+    public delegate PromptResultYesNoCancel YesNoCancelPrompt(string title, string message, PromptType type);
+
+    public enum PromptResultYesNo
+    {
+        No,
+        Yes
+    }
+
+    public enum PromptResultYesNoCancel
+    {
+        No,
+        Yes,
+        Cancel
+    }
+
+    public enum PromptType
+    {
+        Info,
+        Warning,
+        Error
+    }
+}

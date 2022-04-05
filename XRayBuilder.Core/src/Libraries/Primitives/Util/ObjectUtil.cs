cdc29e65144952da71ee5b89238d67b58954c7bb
Ephemerality <revensoftware+github@gmail.com>
2022-04-04 22:34:42 -0300

    fix image methods

--- XRayBuilder.Core/src/Libraries/Primitives/Util/ObjectUtil.cs
+++ XRayBuilder.Core/src/Libraries/Primitives/Util/ObjectUtil.cs
@@ -12,7 +12,7 @@ namespace XRayBuilder.Core.Libraries.Primitives.Util
         /// <typeparam name="T">The type of object being copied.</typeparam>
         /// <param name="source">The object instance to copy.</param>
         /// <returns>The copied object.</returns>
-        public static T Clone<T>(this T source)
+        public static T DeepCopy<T>(this T source)
         {
             // Don't serialize a null object, simply return the default for that object
             if (ReferenceEquals(source, null))
